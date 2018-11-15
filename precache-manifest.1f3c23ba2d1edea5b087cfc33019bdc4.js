ac30f312d6c2d97c383ea5bdcd697905e8b33cc9
oscar <oscar@o2webdev.com>
2018-11-15 09:09:31 -0500

    Updates

---
+++ precache-manifest.1f3c23ba2d1edea5b087cfc33019bdc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactpage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "200b6cbdf1b141780162",
+    "url": "/reactpage/static/js/runtime~main.200b6cbd.js"
+  },
+  {
+    "revision": "0d90efb4f16c5f4602d4",
+    "url": "/reactpage/static/js/main.0d90efb4.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/reactpage/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "0d90efb4f16c5f4602d4",
+    "url": "/reactpage/static/css/main.84b83bb3.chunk.css"
+  },
+  {
+    "revision": "c83f4904bd8befd8ef83b4fd346ee9a0",
+    "url": "/reactpage/index.html"
+  }
+];
